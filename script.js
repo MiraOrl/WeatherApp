46f82f081dd88c52bd78a732e3f379aa0897f7ef
Mira Orlova <mira.orl@bk.ru>
2024-08-20 13:22:49 +0300

    Corrected and supplemented

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
-async function getWeather() {
-  const apiUrl =
-    "https://api.openweathermap.org/data/2.5/weather?q=Moscow&appid=f23899e7b3c9b3e810fb93d62896a99b";
-  console.log(apiUrl);
-  const searchInput = document.querySelector("input");
-  const searchForm = document.querySelector("form");
-  const searchButton = document.querySelector(".search");
+const searchInput = document.querySelector("input");
+const searchButton = document.querySelector(".search");
+
+searchButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  getWeather(searchInput.value);
+  searchInput.value = "";
+});
+
+async function getWeather(city) {
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=f23899e7b3c9b3e810fb93d62896a99b`;
+
   const response = await fetch(apiUrl);
   const weather = await response.json();
 
-  document.querySelector(".city").textContent = weather.name;
+  document.querySelector(".weather-content_city").textContent = weather.name;
   document.querySelector(".weather-content__temperature").innerHTML =
     Math.round(weather.main.temp - 273) + "&deg";
   document.querySelector(".weather-content__text").innerHTML =
@@ -20,11 +25,6 @@ async function getWeather() {
   document.querySelector(".pressure").textContent = weather.main.pressure;
   document.querySelector(".wind-speed").innerHTML =
     Math.round(weather.wind.speed) + "м/с";
-
-  searchButton.addEventListener("click", () => {
-    getWeather(searchInput.value);
-    searchInput.value = "";
-  });
 }
 
-getWeather();
+getWeather("Moscow");
